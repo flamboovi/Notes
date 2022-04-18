e48ee10a5acecbbc2df3df84d6420c37cb30d3a8
flamboovi <102129999+flamboovi@users.noreply.github.com>
2022-04-19 01:20:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+const addButton=document.querySelector('#add');
+
+const updateLSData=()=>{
+    const textAreaData=document.querySelectorAll('textarea');
+    const notes=[];
+
+    textAreaData.forEach((note)=>{
+        return notes.push(note.value);
+    })
+    localStorage.setItem('notes',JSON.stringify(notes));
+}
+
+
+const addNewNote=(text='Sticky Notes')=>{
+    const note=document.createElement('div');
+    note.classList.add('note');
+
+
+    const htmlData=`
+    <div class="operation">
+                <button class="edit"><i class="fas fa-edit"></i></button>
+                <button class="delete"><i class="fas fa-trash"></i></button>
+
+            </div>
+            <div class="main ${text ? "" : "hidden"} "></div>
+            <textarea class="${text ? "hidden" : ""}"></textarea>
+    `;
+    note.insertAdjacentHTML('afterbegin',htmlData);
+
+    const editButton=note.querySelector('.edit');
+    const delButton=note.querySelector('.delete');
+    const mainDiv=note.querySelector('.main');
+    const textarea=note.querySelector('textarea');
+
+    delButton.addEventListener('click',()=>{
+        note.remove();
+    })
+
+    //toggle using edit icon
+textarea.value=text;
+mainDiv.innerHTML=text;
+
+    editButton.addEventListener('click',()=>{
+        mainDiv.classList.toggle('hidden');
+        textarea.classList.toggle('hidden');
+    })
+textarea.addEventListener('change',()=>{
+    const value=event.target.value;
+    mainDiv.innerHTML=value;
+
+    updateLSData();
+})
+    document.body.appendChild(note);
+
+
+}
+
+const notes=JSON.parse(localStorage.getItem('notes'));
+
+if(notes){
+    notes.forEach((note)=>addNewNote(note));
+}
+addButton.addEventListener('click',()=> addNewNote());
